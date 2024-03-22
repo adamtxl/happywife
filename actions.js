94b6b8d0568aa5de74d046bc8636816a8a9246a0
adamtxl <Adamtxl@hotmail.com>
2024-03-22 16:09:39 -0500

    Uploaded index.html, style.css and actions.js.

---
+++ actions.js
@@ -0,0 +1,10 @@
+console.log('My wife is my heart')
+
+// Select the button
+const button = document.querySelector('button');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+  // Display an alert when the button is clicked
+  alert('I love you so much!');
+});
