1d6a4e74896956dbfd8dd5d7bca13b2a3063a0c0
Adam Troxell <adamtxl@hotmail.com>
2024-04-06 17:58:05 -0500

    updated alert to show wedding date. gotta research timezones

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 console.log('My wife is my heart');
 
 var myButton = document.getElementById('myButton');
+const weddingDate = new Date('2022-09-27')
+
 
 myButton.addEventListener('click', function () {
   // Display an alert when the button is clicked
-  alert('I love you so much!');
+  alert('We got marred on '+ weddingDate + '!');
 });
