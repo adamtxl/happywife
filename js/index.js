946cd6e17ef2714499296e75184cffa8cf70761c
Adam Troxell <adamtxl@hotmail.com>
2024-04-06 18:08:48 -0500

    Updated alert again, this shows days since wedding

--- js/index.js
+++ js/index.js
@@ -3,8 +3,16 @@ console.log('My wife is my heart');
 var myButton = document.getElementById('myButton');
 const weddingDate = new Date('2022-09-27')
 
-
 myButton.addEventListener('click', function () {
-  // Display an alert when the button is clicked
-  alert('We got marred on '+ weddingDate + '!');
+  // Get the current date
+  const currentDate = new Date();
+
+  // Calculate the difference in milliseconds between the current date and wedding date
+  const differenceInTime = currentDate.getTime() - weddingDate.getTime();
+
+  // Calculate the difference in days
+  const differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24));
+
+  // Display an alert with the number of days since the wedding date
+  alert('We got married ' + differenceInDays + ' days ago!');
 });
